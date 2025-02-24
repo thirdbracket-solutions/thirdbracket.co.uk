4a5528577de386d05c5ef42a9f672f964e911a44
MUSABBIR SAGAR <56739377+sagarmusabbir@users.noreply.github.com>
2025-02-24 18:06:40 +0600

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -57,11 +57,11 @@ const Hero = () => {
       {/* Background Pattern */}
       <div className="w-full h-full  bg-white dark:bg-gray-950 bg-[linear-gradient(to_right,#80808010_1px,transparent_1px),linear-gradient(to_bottom,#80808010_1px,transparent_1px)] dark:bg-[linear-gradient(to_right,#28282860_1px,transparent_1px),linear-gradient(to_bottom,#28282860_1px,transparent_1px)] bg-[size:24px_24px]">
         <div className="flex flex-col items-center space-y-6 sm:space-y-7 lg:space-y-8">
-          <h1 className="text-[2.70rem]/[2.65rem] sm:text-[3rem]/[2.95rem] lg:text-[3.25rem]/[3.2rem] font-black text-center max-w-3xl sm:max-w-4xl lg:max-w-6xl px-4 bg-gradient-to-r from-gray-800 via-gray-900 to-gray-800 dark:from-gray-200 dark:via-gray-100 dark:to-gray-200 text-transparent bg-clip-text">
+          <h1 className="text-[2.70rem]/[2.70rem] sm:text-[3rem]/[3rem] lg:text-[3.25rem]/[3.25rem] font-black text-center max-w-3xl sm:max-w-4xl lg:max-w-6xl px-4 bg-gradient-to-r from-gray-800 via-gray-900 to-gray-800 dark:from-gray-200 dark:via-gray-100 dark:to-gray-200 text-transparent bg-clip-text">
             Enterprise Solutions, Minus the Overhead Cost
           </h1>
 
-          <p className="text-[0.865rem]/[1.4rem] sm:text-[0.965rem]/[1.5rem] lg:text-[1.065rem]/[1.6rem] text-center text-gray-600 dark:text-gray-300 max-w-2xl sm:max-w-3xl lg:max-w-4xl px-4">
+          <p className="text-[0.865rem]/[1.5rem] sm:text-[0.965rem]/[1.6rem] lg:text-[1.065rem]/[1.7rem] text-center text-gray-600 dark:text-gray-300 max-w-2xl sm:max-w-3xl lg:max-w-4xl px-4">
             Everything you'd expect from an agency—resources, expertise, and
             strategy—at a price you'd normally pay a freelancer. Get custom web
             development and digital marketing designed for startups and growing
